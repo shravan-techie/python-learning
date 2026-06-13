7f44a73be92a508a12576f2b720ecc438c02f98e
shravan-techie <shravan.devtechie@gmail.com>
2026-06-13 09:50:05 +0530

    understand variables and print

---
+++ basics/HelloWorld.py
@@ -0,0 +1,8 @@
+print("Hello World😺")
+print("*"*10)
+print("hello")
+x = 2
+x = 23
+x = 56
+name = "krishna"
+print(name)
---
+++ basics/variables.py
@@ -0,0 +1,20 @@
+
+
+name = "Krishna"    # string
+age = 27            # no
+is_active = False   # boolean
+salary = 120000.00  # float no
+
+
+# print(name)
+# print(age)
+# print(is_active)
+# print(salary)
+
+# print("name: " + name)
+# print("age: " + str(age))
+# print("is_active: " + str(is_active))
+# print("salary: " + str(salary))
+
+# clean way to print
+print(f"Name: {name}\nAge: {age}\nActive: {is_active}\nSalary: {salary}")
